783a77c847d8ea87c7dcebce370295c88a73fc7d
Eliud <58745656+eliudio@users.noreply.github.com>
2023-10-05 12:20:45 +0100

    Extended

--- lib/wizards_package.dart
+++ lib/wizards_package.dart
@@ -1,12 +1,21 @@
 import 'package:eliud_core/core/blocs/access/access_bloc.dart';
 import 'package:eliud_core/core/wizards/registry/registry.dart';
+import 'package:eliud_core/core_package.dart';
+import 'package:eliud_core/eliud.dart';
 import 'package:eliud_core/model/access_model.dart';
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_model.dart';
 import 'package:eliud_core/package/package.dart';
+import 'package:eliud_pkg_etc/etc_package.dart';
+import 'package:eliud_pkg_fundamentals/fundamentals_package.dart';
+import 'package:eliud_pkg_membership/membership_package.dart';
+import 'package:eliud_pkg_pay/pay_package.dart';
+import 'package:eliud_pkg_shop/shop_package.dart';
+import 'package:eliud_pkg_text/text_package.dart';
 import 'package:eliud_pkg_wizards/wizards/membership_workflow_wizard.dart';
 import 'package:eliud_pkg_wizards/wizards/new_policy_from_pdf_wizard.dart';
 import 'package:eliud_pkg_wizards/wizards/new_policy_wizard.dart';
+import 'package:eliud_pkg_workflow/workflow_package.dart';
 import 'wizards/about_page_wizard.dart';
 import 'wizards/blocked_page_wizard.dart';
 
@@ -37,4 +46,18 @@ abstract class WizardsPackage extends Package {
   List<MemberCollectionInfo>? getMemberCollectionInfo() => null;
 
   static WizardsPackage instance() => getWizardsPackage();
+
+  /*
+   * Register depending packages
+   */
+  void registerDependencies(Eliud eliud) {
+    eliud.registerPackage(CorePackage.instance());
+    eliud.registerPackage(TextPackage.instance());
+    eliud.registerPackage(FundamentalsPackage.instance());
+    eliud.registerPackage(EtcPackage.instance());
+    eliud.registerPackage(ShopPackage.instance());
+    eliud.registerPackage(MembershipPackage.instance());
+    eliud.registerPackage(WorkflowPackage.instance());
+    eliud.registerPackage(PayPackage.instance());
+  }
 }
